df0c8cd8701be2ce21f209f971b869f6787bd1c9
Zafar <zufaralievazafarali@gmail.com>
2023-07-31 02:20:38 +0500

    cgjgvv
    rq2rew3r

--- front/src/App.js
+++ front/src/App.js
@@ -1,65 +1,147 @@
 import React, { useState, useEffect, useRef } from 'react';
 import io from 'socket.io-client';
-import "./App.css"
-const socket = io('http://localhost:3001');
+import './App.css';
+
+const socket = io('http://localhost:3001', { autoConnect: false });
+
 function App() {
   const [userID, setUserID] = useState(null);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
+  const [chatPartner, setChatPartner] = useState('');
+  const [users, setUsers] = useState([]);
   const messageRefs = useRef([]);
 
   useEffect(() => {
-    // Слушайте событие chat_history от сервера
+    // Listen for chat_history event from server
     socket.on('chat_history', (data) => {
       setMessages(data);
-      console.log(data);
     });
-    // Слушайте событие receive_message от сервера
+    // Listen for receive_message event from server
     socket.on('receive_message', (data) => {
       setMessages((messages) => [...messages, data.message]);
     });
-    // Слушайте событие update_message от сервера
+    // Listen for update_message event from server
     socket.on('update_message', (data) => {
       setMessages((messages) => messages.map((message) => message.id === data.message.id ? data.message : message));
     });
-    // Очистка обработчиков событий при размонтировании компонента
+    // Listen for users event from server
+    socket.on('users', (data) => {
+      setUsers(data);
+    });
+    // Clean up event handlers when component unmounts
     return () => {
       socket.off('chat_history');
       socket.off('receive_message');
       socket.off('update_message');
+      socket.off('users');
     };
   }, []);
-  
+
   useEffect(() => {
-    // Установка идентификатора пользователя
+    // Set user ID
     setUserID(socket.id);
   }, []);
-  
+
+  const handleRegister = async () => {
+    try {
+      const response = await fetch('http://localhost:3001/register', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password }),
+      });
+      if (response.ok) {
+        alert('User registered successfully!');
+      } else {
+        alert('Error registering user');
+      }
+    } catch (err) {
+      alert('Error registering user');
+    }
+  };
+
+  const handleLogin = async () => {
+    try {
+      const response = await fetch('http://localhost:3001/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password }),
+      });
+      if (response.ok) {
+        const data = await response.json();
+        setUserID(data.id);
+        setUsername(username);
+        socket.auth = { userID: data.id };
+        socket.connect();
+        alert('User logged in successfully!');
+      } else {
+        alert('Invalid credentials');
+      }
+    } catch (err) {
+      alert('Error logging in');
+    }
+  };
+
   const handleSendMessage = () => {
-    // Отправка сообщения на сервер
-    socket.emit('send_message', { message: { text: message } });
-    alert(userID)
+    // Send message to server
+    socket.emit('send_message', { message: { text: message, username } });
     setMessage('');
   };
-  
+
   const handleReadMessage = (messageID) => {
-    // Отправка события read_message на сервер
+    // Send read_message event to server
     socket.emit('read_message', { messageID });
   };
-  
+
+  const handleJoinRoom = () => {
+    // Send join_room event to server
+    socket.emit('join_room', { room: chatPartner });
+  };
+
   return (
     <div className="app">
       <h1>Чат</h1>
+      <div className="auth-container">
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button onClick={handleRegister}>Регистрация</button>
+        <button onClick={handleLogin}>Вход</button>
+      </div>
+      {userID && <p>Вы вошли как: {username}</p>}
+      <div className="users-container">
+        <h2>Пользователи</h2>
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>
+              {user.username}
+              <button onClick={() => setChatPartner(user.id)}>Выбрать</button>
+            </li>
+          ))}
+        </ul>
+        <button onClick={handleJoinRoom}>Присоединиться к комнате</button>
+      </div>
       <ul className="messages">
         {messages.map((message, index) => (
-    <li key={index} ref={(el) => messageRefs.current[index] = el} data-userid={message.userID === userID ? 'own' : 'other'}>
-<p><span>User:   </span>{message.userID}: <p>{message.text}</p> ({new Date(message.timestamp).toLocaleString()})</p>
-
-  <br />
-  {message.userID === userID && <p>Read by: {message.readBy.length}</p>}
-  {message.userID !== userID && <button onClick={() => handleReadMessage(message.id)}>прочитал</button>}
-</li>
-
+          <li key={index} ref={(el) => messageRefs.current[index] = el} data-userid={message.username === username ? 'own' : 'other'}>
+            <p>{message.username}: {message.text} ({new Date(message.timestamp).toLocaleString()})</p>
+            <br />
+            {/* {message.userID === userID && <p>Read by: {message.readBy.length}</p>}
+            {message.userID !== userID && <button onClick={() => handleReadMessage(message.id)}>прочитал</button>} */}
+            {message.username === username && <p>Read by: {message.readBy.length}</p>}
+            {message.username !== username && <button onClick={() => handleReadMessage(message.id)}>прочитал</button>}
+          </li>
         ))}
       </ul>
       <div className="input-container">
@@ -73,7 +155,340 @@ function App() {
     </div>
   );
 }
+
 export default App;
+
+
+// import React, { useState, useEffect, useRef } from 'react';
+// import io from 'socket.io-client';
+// import './App.css';
+
+// const socket = io('http://localhost:3001', { autoConnect: false });
+
+// function App() {
+//   const [userID, setUserID] = useState(null);
+//   const [username, setUsername] = useState('');
+//   const [email, setUsername2] = useState('zafargmail3');
+//   const [password, setPassword] = useState('');
+//   const [message, setMessage] = useState('');
+//   const [messages, setMessages] = useState([]);
+//   const [chatPartner, setChatPartner] = useState('');
+//   const [users, setUsers] = useState([]);
+//   const messageRefs = useRef([]);
+
+//   useEffect(() => {
+//     // Listen for chat_history event from server
+//     socket.on('chat_history', (data) => {
+//       setMessages(data);
+//     });
+//     // Listen for receive_message event from server
+//     socket.on('receive_message', (data) => {
+//       setMessages((messages) => [...messages, data.message]);
+//     });
+//     // Listen for update_message event from server
+//     socket.on('update_message', (data) => {
+//       setMessages((messages) => messages.map((message) => message.id === data.message.id ? data.message : message));
+//     });
+//     // Listen for users event from server
+//     socket.on('users', (data) => {
+//       setUsers(data);
+//       console.log(data,"aaaaaaaaaaaaaaaa");
+//     });
+//     // Clean up event handlers when component unmounts
+//     return () => {
+//       socket.off('chat_history');
+//       socket.off('receive_message');
+//       socket.off('update_message');
+//       socket.off('users');
+//     };
+//   }, []);
+
+//   useEffect(() => {
+//     // Set user ID
+//     setUserID(socket.id);
+//   }, []);
+
+//   const handleRegister = async () => {
+//     try {
+//       const response = await fetch('http://localhost:3001/register', {
+//         method: 'POST',
+//         headers: { 'Content-Type': 'application/json' },
+//         body: JSON.stringify({ username, password,email }),
+//       });
+//       if (response.ok) {
+//         alert('User registered successfully!');
+//       } else {
+//         alert('Error registering user');
+//       }
+//     } catch (err) {
+//       alert('Error registering user');
+//     }
+//   };
+
+//   const handleLogin = async () => {
+//     try {
+//       const response = await fetch('http://localhost:3001/login', {
+//         method: 'POST',
+//         headers: { 'Content-Type': 'application/json' },
+//         body: JSON.stringify({ username, password }),
+//       });
+//       if (response.ok) {
+//         const data = await response.json();
+//         setUserID(data.id);
+//         socket.auth = { userID: data.id };
+//         socket.connect();
+//         alert('User logged in successfully!');
+//       } else {
+//         alert('Invalid credentials');
+//       }
+//     } catch (err) {
+//       alert('Error logging in');
+//     }
+//   };
+
+//   const handleSendMessage = () => {
+//     // Send message to server
+//     socket.emit('send_message', { message: { text: message } });
+//     setMessage('');
+//   };
+
+//   const handleReadMessage = (messageID) => {
+//     // Send read_message event to server
+//     socket.emit('read_message', { messageID });
+//   };
+
+//   const handleJoinRoom = () => {
+//     // Send join_room event to server
+//     socket.emit('join_room', { room: chatPartner });
+//   };
+
+//   return (
+//     <div className="app">
+//       <h1>Чат</h1>
+//       <div className="auth-container">
+//         <input
+//           type="text"
+//           placeholder="Username"
+//           value={username}
+//           onChange={(e) => setUsername(e.target.value)}
+//         />
+//         <input
+//           type="password"
+//           placeholder="Password"
+//           value={password}
+//           onChange={(e) => setPassword(e.target.value)}
+//         />
+//         <button onClick={handleRegister}>Регистрация</button>
+//         <button onClick={handleLogin}>Вход</button>
+//       </div>
+//       {userID && <p>Вы вошли как: {username}</p>}
+//       <div className="users-container">
+//         <h2>Пользователи</h2>
+//         <ul>
+//           {users.map((user) => (
+//             <li key={user.id}>
+//               {user.username}
+//               <button onClick={() => setChatPartner(user.id)}>Выбрать</button>
+//             </li>
+//           ))}
+//         </ul>
+//         <button onClick={handleJoinRoom}>Присоединиться к комнате</button>
+//       </div>
+//       <ul className="messages">
+//         {messages.map((message, index) => (
+//           <li key={index} ref={(el) => messageRefs.current[index] = el} data-userid={message.userID === userID ? 'own' : 'other'}>
+//             <p>{message.userID}: {message.text} ({new Date(message.timestamp).toLocaleString()})</p>
+//             <br />
+//             {message.userID === userID && <p>Read by: {message.readBy.length}</p>}
+//             {message.userID !== userID && <button onClick={() => handleReadMessage(message.id)}>прочитал</button>}
+//           </li>
+//         ))}
+//       </ul>
+//       <div className="input-container">
+//         <input
+//           type="text"
+//           value={message}
+//           onChange={(e) => setMessage(e.target.value)}
+//         />
+//         <button onClick={handleSendMessage}>Отправить</button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState, useEffect, useRef } from 'react';
+// import io from 'socket.io-client';
+// import './App.css';
+
+// const socket = io('http://localhost:3000', { autoConnect: false });
+
+// function App() {
+//   const [userID, setUserID] = useState(null);
+//   const [username, setUsername] = useState('');
+//   const [email, setEmail] = useState('zafaremail');
+//   const [password, setPassword] = useState('');
+//   const [message, setMessage] = useState('');
+//   const [messages, setMessages] = useState([]);
+//   const [chatPartner, setChatPartner] = useState('');
+//   const [users, setUsers] = useState([]);
+//   const messageRefs = useRef([]);
+
+//   useEffect(() => {
+//     // Listen for chat_history event from server
+//     socket.on('chat_history', (data) => {
+//       setMessages(data);
+//     });
+//     // Listen for receive_message event from server
+//     socket.on('receive_message', (data) => {
+//       setMessages((messages) => [...messages, data.message]);
+//     });
+//     // Listen for update_message event from server
+//     socket.on('update_message', (data) => {
+//       setMessages((messages) => messages.map((message) => message.id === data.message.id ? data.message : message));
+//     });
+//     // Listen for users event from server
+//     socket.on('users', (data) => {
+//       setUsers(data);
+//     });
+//     // Clean up event handlers when component unmounts
+//     return () => {
+//       socket.off('chat_history');
+//       socket.off('receive_message');
+//       socket.off('update_message');
+//       socket.off('users');
+//     };
+//   }, []);
+
+//   useEffect(() => {
+//     // Set user ID
+//     setUserID(socket.id);
+//   }, []);
+
+//   const handleRegister = async () => {
+//     try {
+//       const response = await fetch('http://localhost:3001/register', {
+//         method: 'POST',
+//         headers: { 'Content-Type': 'application/json' },
+//         body: JSON.stringify({ username, password,email }),
+//       });
+//       if (response.ok) {
+//         alert('User registered successfully!');
+//       } else {
+//         alert('Error registering user');
+//       }
+//     } catch (err) {
+//       alert('Error registering user dd');
+//     }
+//   };
+
+//   const handleLogin = async () => {
+//     try {
+//       const response = await fetch('http://localhost:3001/login', {
+//         method: 'POST',
+//         headers: { 'Content-Type': 'application/json' },
+//         body: JSON.stringify({ username, password }),
+//       });
+//       if (response.ok) {
+//         const data = await response.json();
+//         setUserID(data.id);
+//         socket.auth = { userID: data.id };
+//         socket.connect();
+//         alert('User logged in successfully!');
+//       } else {
+//         alert('Invalid credentials');
+//       }
+//     } catch (err) {
+//       alert('Error logging in');
+//     }
+//   };
+
+//   const handleSendMessage = () => {
+//     // Send message to server
+//     socket.emit('send_message', { message: { text: message } });
+//     setMessage('');
+//   };
+
+//   const handleReadMessage = (messageID) => {
+//     // Send read_message event to server
+//     socket.emit('read_message', { messageID });
+//   };
+
+//   const handleJoinRoom = () => {
+//     // Send join_room event to server
+//     socket.emit('join_room', { room: chatPartner });
+//   };
+
+//   return (
+//     <div className="app">
+//       <h1>Чат</h1>
+//       <div className="auth-container">
+//         <input
+//           type="text"
+//           placeholder="Username"
+//           value={username}
+//           onChange={(e) => setUsername(e.target.value)}
+//         />
+//         <input
+//           type="password"
+//           placeholder="Password"
+//           value={password}
+//           onChange={(e) => setPassword(e.target.value)}
+//         />
+//         <button onClick={handleRegister}>Регистрация</button>
+//         <button onClick={handleLogin}>Вход</button>
+//       </div>
+//       <div className="users-container">
+//         <h2>Пользователи</h2>
+//         <ul>
+//           {users.map((user) => (
+//             <li key={user.id}>
+//               {user.username}
+//               <button onClick={() => setChatPartner(user.id)}>Выбрать</button>
+//             </li>
+//           ))}
+//         </ul>
+//         <button onClick={handleJoinRoom}>Присоединиться к комнате</button>
+//       </div>
+//       <ul className="messages">
+//         {messages.map((message, index) => (
+//           <li key={index} ref={(el) => messageRefs.current[index] = el} data-userid={message.userID === userID ? 'own' : 'other'}>
+//             <p>{message.userID}: {message.text} ({new Date(message.timestamp).toLocaleString()})</p>
+//             <br />
+//             {message.userID === userID && <p>Read by: {message.readBy.length}</p>}
+//             {message.userID !== userID && <button onClick={() => handleReadMessage(message.id)}>прочитал</button>}
+//           </li>
+//         ))}
+//       </ul>
+//       <div className="input-container">
+//         <input
+//           type="text"
+//           value={message}
+//           onChange={(e) => setMessage(e.target.value)}
+//         />
+//         <button onClick={handleSendMessage}>Отправить</button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 
 
 
--- server/index.js
+++ server/index.js
@@ -3,7 +3,11 @@ const app = express();
 const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
+const bcrypt = require('bcrypt');
+const { Pool } = require('pg');
+
 app.use(cors());
+app.use(express.json());
 
 const server = http.createServer(app);
 
@@ -11,53 +15,113 @@ const io = new Server(server, {
   cors: { origin: 'http://localhost:3000', methods: ['GET', 'POST'] }
 });
 
-// Хранение истории чата и идентификаторов групп
+// Set up connection to PostgreSQL database
+const db = new Pool({
+  host: 'localhost',
+  user: 'postgres',
+  password: 'FREEFREEf1',
+  database: 'postgres',
+  port: 5432
+});
+
+// Store chat history and group IDs
 let chatHistory = {};
 let groupIDs = {};
 
-io.on('connection', (socket) => {
-  console.log(`Пользователь подключен: ${socket.id}`);
+// Routes for user registration and login
+app.post('/register', async (req, res) => {
+  const { username, password,email } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  try {
+    await db.query('INSERT INTO users (username, password,email) VALUES ($1, $2,$3)', [username, hashedPassword,email]);
+    res.status(201).send('User created');
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
 
-  // Назначение клиента группе
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const result = await db.query('SELECT * FROM users WHERE username = $1', [username]);
+    if (result.rows.length > 0) {
+      const user = result.rows[0];
+      const match = await bcrypt.compare(password, user.password);
+      if (match) {
+        res.status(200).send({ id: user.id });
+      } else {
+        res.status(401).send('Invalid credentials');
+      }
+    } else {
+      res.status(401).send('Invalid credentials');
+    }
+  } catch (err) {
+    res.status(500).send('Error logging in');
+  }
+});
+
+// Authenticate users when connecting to socket.io server
+io.use(async (socket, next) => {
+  const userID = socket.handshake.auth.userID;
+  if (!userID) {
+    return next(new Error('Invalid credentials'));
+  }
+  try {
+    const result = await db.query('SELECT * FROM users WHERE id = $1', [userID]);
+    if (result.rows.length > 0) {
+      socket.userID = userID;
+      next();
+    } else {
+      next(new Error('Invalid credentials'));
+    }
+  } catch (err) {
+    next(new Error('Error authenticating user'));
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+  // Assign client to group
   groupIDs[socket.id] = 'group1';
   socket.join('group1');
 
-  // Отправка истории чата клиенту
+  // Send chat history to client
   socket.emit('chat_history', chatHistory['group1'] || []);
 
-  // Слушайте событие send_message от клиента
+  // Listen for send_message event from client
   socket.on('send_message', (data) => {
-    // Добавление даты и времени отправки сообщения
+    // Add timestamp to message
     data.message.timestamp = new Date();
-    // Добавление идентификатора пользователя
+    // Add user ID to message
     data.message.userID = socket.id;
-    // Добавление поля readBy
+    // Add readBy field to message
     data.message.readBy = [];
-    // Добавление уникального идентификатора сообщения
+    // Add unique message ID
     data.message.id = Date.now();
   
-    // Сохранение сообщения в истории чата
+    // Save message to chat history
     let groupID = groupIDs[socket.id];
     if (!chatHistory[groupID]) {
       chatHistory[groupID] = [];
     }
     chatHistory[groupID].push(data.message);
-    // Трансляция сообщения всем клиентам в комнате
+    // Broadcast message to all clients in room
     io.to(groupID).emit('receive_message', data);
   });
   
-  // Слушайте событие join_room от клиента
+  // Listen for join_room event from client
   socket.on('join_room', (data) => {
-    // Присоединение к комнате
+    // Join room
     let groupID = data.room;
     groupIDs[socket.id] = groupID;
     socket.join(groupID);
 
-    // Отправка истории чата клиенту
+    // Send chat history to client
     socket.emit('chat_history', chatHistory[groupID] || []);
   });
 
-  // Слушайте событие read_message от клиента
+  // Listen for read_message event from client
   socket.on('read_message', (data) => {
     let groupID = groupIDs[socket.id];
     let messageIndex = chatHistory[groupID].findIndex((message) => message.id === data.messageID);
@@ -70,8 +134,9 @@ io.on('connection', (socket) => {
 });
 
 server.listen(3001, () => {
-  console.log('Сервер запущен');
+  console.log('Server started');
 });
+
 
 
 
