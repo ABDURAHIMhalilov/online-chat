cb9fae3ae4b9d78347d1d1dfe728f5608834ab8d
Zafar <zufaralievazafarali@gmail.com>
2023-07-30 20:11:59 +0500

    egg
    egeg

--- front/src/App.js
+++ front/src/App.js
@@ -1,29 +1,138 @@
-import logo from './logo.svg';
-import './App.css';
-import io from "socket.io-client";
-import { useEffect, useState } from 'react';
-const socket=io.connect('http://localhost:3001');
+import React, { useState, useEffect, useRef } from 'react';
+import io from 'socket.io-client';
+import "./App.css"
+const socket = io('http://localhost:3001');
 function App() {
-  const [message,setMessage]=useState('')
-  const [messageF,setMessageF]=useState('')
+  const [userID, setUserID] = useState(null);
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+  const messageRefs = useRef([]);
 
-  const sendMessage=()=>{
-   socket.emit('send_message',{message:message})
-  }
-useEffect(()=>{
-socket.on('receive_message',(data)=>{
-  setMessageF(data.message)
-  // console.log(data.message);
-})
-},[socket])
-
+  useEffect(() => {
+    // Слушайте событие chat_history от сервера
+    socket.on('chat_history', (data) => {
+      setMessages(data);
+      console.log(data);
+    });
+    // Слушайте событие receive_message от сервера
+    socket.on('receive_message', (data) => {
+      setMessages((messages) => [...messages, data.message]);
+    });
+    // Слушайте событие update_message от сервера
+    socket.on('update_message', (data) => {
+      setMessages((messages) => messages.map((message) => message.id === data.message.id ? data.message : message));
+    });
+    // Очистка обработчиков событий при размонтировании компонента
+    return () => {
+      socket.off('chat_history');
+      socket.off('receive_message');
+      socket.off('update_message');
+    };
+  }, []);
+  
+  useEffect(() => {
+    // Установка идентификатора пользователя
+    setUserID(socket.id);
+  }, []);
+  
+  const handleSendMessage = () => {
+    // Отправка сообщения на сервер
+    socket.emit('send_message', { message: { text: message } });
+    alert(userID)
+    setMessage('');
+  };
+  
+  const handleReadMessage = (messageID) => {
+    // Отправка события read_message на сервер
+    socket.emit('read_message', { messageID });
+  };
+  
   return (
-    <div className="App">
-  <input placeholder="Message..." onChange={(event)=>{setMessage(event.target.value)}} />
-  <button onClick={sendMessage}>Send Message</button>
-  <h1>Message: {messageF} </h1>
+    <div className="app">
+      <h1>Чат</h1>
+      <ul className="messages">
+        {messages.map((message, index) => (
+    <li key={index} ref={(el) => messageRefs.current[index] = el} data-userid={message.userID === userID ? 'own' : 'other'}>
+<p><span>User:   </span>{message.userID}: <p>{message.text}</p> ({new Date(message.timestamp).toLocaleString()})</p>
+
+  <br />
+  {message.userID === userID && <p>Read by: {message.readBy.length}</p>}
+  {message.userID !== userID && <button onClick={() => handleReadMessage(message.id)}>прочитал</button>}
+</li>
+
+        ))}
+      </ul>
+      <div className="input-container">
+        <input
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button onClick={handleSendMessage}>Отправить</button>
+      </div>
     </div>
   );
 }
-
 export default App;
+
+
+
+
+
+// import React, { useState, useEffect, useRef } from 'react';
+// import io from 'socket.io-client';
+// import "./App.css"
+// const socket = io('http://localhost:3001');
+// function App() {
+//   const [message, setMessage] = useState('');
+//   const [messages, setMessages] = useState([]);
+//   const messageRefs = useRef([]);
+
+//   useEffect(() => {
+//     // Слушайте событие chat_history от сервера
+//     socket.on('chat_history', (data) => {
+//       setMessages(data);
+//       console.log(data);
+//     });
+//     // Слушайте событие receive_message от сервера
+//     socket.on('receive_message', (data) => {
+//       setMessages((messages) => [...messages, data.message]);
+//     });
+//     // Очистка обработчиков событий при размонтировании компонента
+//     return () => {
+//       socket.off('chat_history');
+//       socket.off('receive_message');
+//     };
+//   }, []);
+  
+//   const handleSendMessage = () => {
+//     // Отправка сообщения на сервер
+//     socket.emit('send_message', { message: { text: message } });
+//     setMessage('');
+//   };
+  
+  
+
+  
+//   return (
+//     <div>
+//       <h1>Чат</h1>
+//       <ul>
+//         {messages.map((message, index) => (
+//           <li key={index} ref={(el) => messageRefs.current[index] = el}>
+//             <p><span>User:   </span>{message.userID}</p>: {message.text} ({new Date(message.timestamp).toLocaleString()})
+//             <br />
+//           </li>
+//         ))}
+//       </ul>
+//       <input
+//         type="text"
+//         value={message}
+//         onChange={(e) => setMessage(e.target.value)}
+//       />
+//       <button onClick={handleSendMessage}>Отправить</button>
+//     </div>
+//   );
+// }
+// export default App;
+
--- server/index.js
+++ server/index.js
@@ -1,24 +1,139 @@
-const express=require("express");
-const app=express();
-const http=require('http');
-const { Server }=require('socket.io')
-const cors=require('cors')
-app.use(cors())
+const express = require('express');
+const app = express();
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+app.use(cors());
 
-const server=http.createServer(app);
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: { origin: 'http://localhost:3000', methods: ['GET', 'POST'] }
+});
+
+// Хранение истории чата и идентификаторов групп
+let chatHistory = {};
+let groupIDs = {};
+
+io.on('connection', (socket) => {
+  console.log(`Пользователь подключен: ${socket.id}`);
+
+  // Назначение клиента группе
+  groupIDs[socket.id] = 'group1';
+  socket.join('group1');
+
+  // Отправка истории чата клиенту
+  socket.emit('chat_history', chatHistory['group1'] || []);
+
+  // Слушайте событие send_message от клиента
+  socket.on('send_message', (data) => {
+    // Добавление даты и времени отправки сообщения
+    data.message.timestamp = new Date();
+    // Добавление идентификатора пользователя
+    data.message.userID = socket.id;
+    // Добавление поля readBy
+    data.message.readBy = [];
+    // Добавление уникального идентификатора сообщения
+    data.message.id = Date.now();
+  
+    // Сохранение сообщения в истории чата
+    let groupID = groupIDs[socket.id];
+    if (!chatHistory[groupID]) {
+      chatHistory[groupID] = [];
+    }
+    chatHistory[groupID].push(data.message);
+    // Трансляция сообщения всем клиентам в комнате
+    io.to(groupID).emit('receive_message', data);
+  });
+  
+  // Слушайте событие join_room от клиента
+  socket.on('join_room', (data) => {
+    // Присоединение к комнате
+    let groupID = data.room;
+    groupIDs[socket.id] = groupID;
+    socket.join(groupID);
+
+    // Отправка истории чата клиенту
+    socket.emit('chat_history', chatHistory[groupID] || []);
+  });
+
+  // Слушайте событие read_message от клиента
+  socket.on('read_message', (data) => {
+    let groupID = groupIDs[socket.id];
+    let messageIndex = chatHistory[groupID].findIndex((message) => message.id === data.messageID);
+    
+    if (messageIndex !== -1) {
+      chatHistory[groupID][messageIndex].readBy.push(socket.id);
+      io.to(groupID).emit('update_message', { message: chatHistory[groupID][messageIndex] });
+    }
+  });
+});
+
+server.listen(3001, () => {
+  console.log('Сервер запущен');
+});
 
 
-const io=new Server(server,{
-    cors:{orgin:'http://localhost:3000',methods:['GET','POST']}
-})
-io.on('connection',(socket)=>{
-    console.log(`User connection ${socket.id}`);
 
-socket.on('send_message',(data)=>{
-    // console.log(data);
-    socket.broadcast.emit("receive_message", data )
-}) 
-})
-server.listen(3001,()=>{
-    console.log("sever runing")
-})
+
+// const express = require('express');
+// const app = express();
+// const http = require('http');
+// const { Server } = require('socket.io');
+// const cors = require('cors');
+// app.use(cors());
+
+// const server = http.createServer(app);
+
+// const io = new Server(server, {
+//   cors: { origin: 'http://localhost:3000', methods: ['GET', 'POST'] }
+// });
+
+// // Хранение истории чата и идентификаторов групп
+// let chatHistory = {};
+// let groupIDs = {};
+
+// io.on('connection', (socket) => {
+//   console.log(`Пользователь подключен: ${socket.id}`);
+
+//   // Назначение клиента группе
+//   groupIDs[socket.id] = 'group1';
+//   socket.join('group1');
+
+//   // Отправка истории чата клиенту
+//   socket.emit('chat_history', chatHistory['group1'] || []);
+
+//   // Слушайте событие send_message от клиента
+//   socket.on('send_message', (data) => {
+//     // Добавление даты и времени отправки сообщения
+//     data.message.timestamp = new Date();
+//     // Добавление идентификатора пользователя
+//     data.message.userID = socket.id;
+//     // Добавление поля readBy
+//     data.message.readBy = [];
+  
+//     // Сохранение сообщения в истории чата
+//     let groupID = groupIDs[socket.id];
+//     if (!chatHistory[groupID]) {
+//       chatHistory[groupID] = [];
+//     }
+//     chatHistory[groupID].push(data.message);
+//     // Трансляция сообщения всем клиентам в комнате
+//     io.to(groupID).emit('receive_message', data);
+//   });
+  
+//   // Слушайте событие join_room от клиента
+//   socket.on('join_room', (data) => {
+//     // Присоединение к комнате
+//     let groupID = data.room;
+//     groupIDs[socket.id] = groupID;
+//     socket.join(groupID);
+
+//     // Отправка истории чата клиенту
+//     socket.emit('chat_history', chatHistory[groupID] || []);
+//   });
+// });
+
+// server.listen(3001, () => {
+//   console.log('Сервер запущен');
+// });
